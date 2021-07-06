e67525ac8411bb1d7554fffb9ea299cf12526457
parasco1 <PARASCHIV.VALENTIN@YMAIL.COM>
2021-07-06 21:37:59 +0300

    stase DM

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -39,7 +39,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq88llllsgzgw32',
 };
 
 const networkSchema = object({
